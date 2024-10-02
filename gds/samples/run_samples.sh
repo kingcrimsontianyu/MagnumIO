9bfdafe2d0e9f94d41d1d663efad8143b254085a
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-10-02 14:26:07 +0000

    Update

--- gds/samples/run_samples.sh
+++ gds/samples/run_samples.sh
@@ -22,114 +22,118 @@ print_test_idx() {
     ((test_idx += 1))
 }
 
-print_test_idx
 filepath=foo
 device_id=0
-$sample_dir/cufile_sample_001 $filepath $device_id
+$sample_dir/cufile_sample_biu $filepath $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_002 $filepath $device_id
+# print_test_idx
+# filepath=foo
+# device_id=0
+# $sample_dir/cufile_sample_001 $filepath $device_id
 
-print_test_idx
-filepath_1=foo1
-filepath_2=foo2
-$sample_dir/cufile_sample_003 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_002 $filepath $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_004 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# filepath_1=foo1
+# filepath_2=foo2
+# $sample_dir/cufile_sample_003 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_005 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_004 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_006 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_005 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_007
+# print_test_idx
+# $sample_dir/cufile_sample_006 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_008
+# print_test_idx
+# $sample_dir/cufile_sample_007
 
-print_test_idx
-$sample_dir/cufile_sample_009 $filepath_1 $filepath_2
+# print_test_idx
+# $sample_dir/cufile_sample_008
 
-print_test_idx
-$sample_dir/cufile_sample_010 $filepath $device_id $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_009 $filepath_1 $filepath_2
 
-print_test_idx
-$sample_dir/cufile_sample_011 $filepath $device_id $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_010 $filepath $device_id $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_012 $filepath_1 $filepath_2
+# print_test_idx
+# $sample_dir/cufile_sample_011 $filepath $device_id $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_013 $filepath_1 $filepath_2
+# print_test_idx
+# $sample_dir/cufile_sample_012 $filepath_1 $filepath_2
 
-print_test_idx
-$sample_dir/cufile_sample_014 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_013 $filepath_1 $filepath_2
 
-print_test_idx
-# mode: DeviceMemory = 1, ManagedMemory = 2, HostMemory = 3
-mode=1
-$sample_dir/cufile_sample_015 $filepath_1 $filepath_2 $device_id $mode
+# print_test_idx
+# $sample_dir/cufile_sample_014 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_016 $filepath
+# print_test_idx
+# # mode: DeviceMemory = 1, ManagedMemory = 2, HostMemory = 3
+# mode=1
+# $sample_dir/cufile_sample_015 $filepath_1 $filepath_2 $device_id $mode
 
-print_test_idx
-$sample_dir/cufile_sample_017 $filepath
+# print_test_idx
+# $sample_dir/cufile_sample_016 $filepath
 
-print_test_idx
-$sample_dir/cufile_sample_018 $filepath
+# print_test_idx
+# $sample_dir/cufile_sample_017 $filepath
 
-print_test_idx
-num_batch_entries=4
-$sample_dir/cufile_sample_019 $filepath $device_id $num_batch_entries
+# print_test_idx
+# $sample_dir/cufile_sample_018 $filepath
 
-print_test_idx
-$sample_dir/cufile_sample_020 $filepath $device_id $num_batch_entries
+# print_test_idx
+# num_batch_entries=4
+# $sample_dir/cufile_sample_019 $filepath $device_id $num_batch_entries
 
-print_test_idx
-$sample_dir/cufile_sample_021 $filepath $device_id $num_batch_entries
+# print_test_idx
+# $sample_dir/cufile_sample_020 $filepath $device_id $num_batch_entries
 
-print_test_idx
-non_direct_flag=0
-$sample_dir/cufile_sample_022 $filepath $device_id $non_direct_flag
+# print_test_idx
+# $sample_dir/cufile_sample_021 $filepath $device_id $num_batch_entries
 
-print_test_idx
-$sample_dir/cufile_sample_023 $filepath_1 $filepath_2
+# print_test_idx
+# non_direct_flag=0
+# $sample_dir/cufile_sample_022 $filepath $device_id $non_direct_flag
 
-print_test_idx
-$sample_dir/cufile_sample_024 $filepath
+# print_test_idx
+# $sample_dir/cufile_sample_023 $filepath_1 $filepath_2
 
-print_test_idx
-$sample_dir/cufile_sample_025 $filepath_1 $filepath_2
+# print_test_idx
+# $sample_dir/cufile_sample_024 $filepath
 
-print_test_idx
-$sample_dir/cufile_sample_026 $filepath_1 $filepath_2
+# print_test_idx
+# $sample_dir/cufile_sample_025 $filepath_1 $filepath_2
 
-print_test_idx
-$sample_dir/cufile_sample_027 $filepath $device_id $num_batch_entries $non_direct_flag
+# print_test_idx
+# $sample_dir/cufile_sample_026 $filepath_1 $filepath_2
 
-print_test_idx
-$sample_dir/cufile_sample_028 $filepath $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_027 $filepath $device_id $num_batch_entries $non_direct_flag
 
-print_test_idx
-$sample_dir/cufile_sample_029 $filepath $device_id $num_batch_entries $non_direct_flag
+# print_test_idx
+# $sample_dir/cufile_sample_028 $filepath $device_id
 
-print_test_idx
-# Buf Register 0 - register all buffers, 1 - unregistered buffers
-buf_register_flag=0
-$sample_dir/cufile_sample_030 $filepath $device_id $num_batch_entries $buf_register_flag $non_direct_flag
+# print_test_idx
+# $sample_dir/cufile_sample_029 $filepath $device_id $num_batch_entries $non_direct_flag
 
-print_test_idx
-$sample_dir/cufile_sample_031 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# # Buf Register 0 - register all buffers, 1 - unregistered buffers
+# buf_register_flag=0
+# $sample_dir/cufile_sample_030 $filepath $device_id $num_batch_entries $buf_register_flag $non_direct_flag
 
-print_test_idx
-$sample_dir/cufile_sample_032 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_031 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_033 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_032 $filepath_1 $filepath_2 $device_id
 
-print_test_idx
-$sample_dir/cufile_sample_034 $filepath_1 $filepath_2 $device_id
+# print_test_idx
+# $sample_dir/cufile_sample_033 $filepath_1 $filepath_2 $device_id
+
+# print_test_idx
+# $sample_dir/cufile_sample_034 $filepath_1 $filepath_2 $device_id
